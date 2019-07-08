cc9d211f84b50c6b8e5a01c921080a81fc4e731e
Sangmin Yoon <sangmin.yoon@croquis.com>
2019-07-09 04:09:18 +0900

    user: start express server

---
+++ user/app/app.ts
@@ -0,0 +1,14 @@
+import { createApp } from '@croquiscom/crary-express';
+import http from 'http';
+import { Config } from '../config';
+
+const app = createApp({
+  project_root: Config.project_root,
+  routers: {},
+});
+const server = http.createServer(app);
+
+server.listen(Config.port, () => {
+  const worker_num = process.env.NODE_APP_INSTANCE || 0;
+  console.log(`[${new Date().toISOString()}] [${Config.project}.services #${worker_num}] Started at http://localhost:${Config.port}`);
+});
---
+++ user/config/index.ts
@@ -0,0 +1,7 @@
+import path from 'path';
+
+export const Config = {
+  project: 'user',
+  project_root: path.resolve(__dirname, '..'),
+  port: 6400,
+};
